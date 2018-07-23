7bde0d4ade5be4cc1b6c8fab1d4e44950175eb82
the-c0d3r <the-c0d3r@users.noreply.github.com>
2018-07-23 08:14:06 +0800

    Added url class

--- lib/connection.py
+++ lib/connection.py
@@ -29,6 +29,21 @@ class HTTP(object):
             "response" : request.text
         }
 
+class URL(object):
+    """A url class to handle all the URL related operation"""
+    def __init__(self, url):
+        """initialize URL object"""
+        if url.startswith("http://"):
+            self.fullurl = url
+        else:
+            self.fullurl = "http://" + url
+        if not self.fullurl.endswith("/"):
+            self.fullurl += "/"
+
+    def geturl(self):
+        """Get the formatted url"""
+        return self.fullurl
+
 
 class URLHandler(HTTP):
     """General URL handler"""
