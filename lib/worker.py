146c38d976f7ad9a928fbedc1002f683f88b3458
the-c0d3r <z3r0.mhu@gmail.com>
2018-10-14 19:03:23 +0800

    lib/worker.py: added function annotations

--- lib/worker.py
+++ lib/worker.py
@@ -1,24 +1,24 @@
 import threading
 import logging
+import queue
 
-from lib.connection import  URLHandler
+from lib.connection import URLHandler
 
 
 class WorkerThread(threading.Thread):
     """The threads that will be processing all the wordlist items"""
-    def __init__(self, queue):
+    def __init__(self, queue: queue.Queue) -> None:
         super().__init__()
         self.queue = queue
         self.urlHandler = URLHandler()
         self.logger = logging.getLogger("admin-finder")
         self.work = True
 
-    def run(self):
+    def run(self) -> None:
         try:
             while self.work:
                 url = self.queue.get()
                 if self.urlHandler.scan(url) == 200:
-                    # TODO: check if it will return int or string
                     self.logger.info("Admin page found: %s", url)
         except KeyboardInterrupt:
             self.logger.info("Ctrl+C Detected in Thread")
