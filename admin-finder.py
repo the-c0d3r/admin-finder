ebd8687eab0b131b0f06b078c20d5a6150652705
the-c0d3r <4526565+the-c0d3r@users.noreply.github.com>
2021-05-19 23:07:05 +0800

    Fixed threadcount issue, implemented semaphore

--- admin-finder.py
+++ admin-finder.py
@@ -1,26 +1,26 @@
-import aiohttp
 import argparse
 import asyncio
 import logging
 import random
-import sys
 import time
-
 from typing import Optional
 
+import aiohttp
+from aiohttp import ClientResponse
 
-from lib.wordlist import WordListGenerator
 from lib.robot import RobotHandler
-
+from lib.wordlist import WordListGenerator
 
 AGENT_FILE = "config/agents.ini"
 
 
-async def fetch(session: aiohttp.ClientSession, url: str, semaphore: asyncio.Semaphore) -> Optional[str]:
+async def fetch(session: aiohttp.ClientSession, url: str, semaphore: asyncio.Semaphore) -> ClientResponse:
     """Fetch the url with the semaphore and return the response"""
     try:
-        async with session.get(url, timeout=15) as response:
+        await semaphore.acquire()
+        async with session.get(url, timeout = 15) as response:
             if response.status != 404:
+                semaphore.release()
                 return response
     except aiohttp.ClientResponseError as e:
         logging.warning(e.code)
@@ -28,6 +28,8 @@ async def fetch(session: aiohttp.ClientSession, url: str, semaphore: asyncio.Sem
         logging.warning("Timeout")
     except Exception as e:
         logging.warning(e)
+    finally:
+        semaphore.release()
 
 
 async def fetch_async(urls: [str], semaphore: asyncio.Semaphore) -> [Optional[str]]:
@@ -35,7 +37,7 @@ async def fetch_async(urls: [str], semaphore: asyncio.Semaphore) -> [Optional[st
     tasks = []
     header = {"User-Agent": random.choice(load_agents())}
 
-    async with aiohttp.ClientSession(headers=header) as session:
+    async with aiohttp.ClientSession(headers = header) as session:
         for url in urls:
             task = asyncio.ensure_future(fetch(session, url, semaphore))
             tasks.append(task)
@@ -52,10 +54,11 @@ def load_agents() -> [str]:
 
 
 def build_parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser(prog="admin-finder.py", description="Admin panel finder")
-    parser.add_argument("-u", "--url", help="Target url/website")
-    parser.add_argument("-w", "--wordlist", help="Wordlist to use, default 'wordlist.txt'", default = "wordlists/wordlist.txt")
-    parser.add_argument("-t", "--threadcount", help="Number of threads to use", default = 1000)
+    parser = argparse.ArgumentParser(prog = "admin-finder.py", description = "Admin panel finder")
+    parser.add_argument("-u", "--url", help = "Target url/website")
+    parser.add_argument("-w", "--wordlist", help = "Wordlist to use, default 'wordlist.txt'",
+        default = "wordlists/wordlist.txt")
+    parser.add_argument("-t", "--threadcount", help = "Number of threads to use", default = 1000)
     return parser
 
 
@@ -81,6 +84,11 @@ def main() -> None:
         print("[-] -u URL paremeter required")
         exit()
 
+    if not args.url.startswith("https://"):
+        args.url = "https://" + args.url
+
+    args.threadcount = int(args.threadcount)
+
     # scan for robot file
     robot_handler = RobotHandler(args.url)
     result = robot_handler.scan()
@@ -98,6 +106,8 @@ def main() -> None:
     try:
         semaphore = asyncio.Semaphore(args.threadcount)
         urls = WordListGenerator(args.url, args.wordlist)
+
+        print(f"[+] Scanning {urls.max} urls with {args.threadcount} threads")
 
         start = time.time()
         loop = asyncio.get_event_loop()
